3119eeb6d87042ff1989f51e2babec33f644ca78
quangngo <banbanhbeo@outlook.com>
2026-05-03 11:23:24 +0700

    add server constant

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -1,6 +1,6 @@
 class ServerConstant {
   static const String serverURL = String.fromEnvironment(
     'SERVER_URL',
-    defaultValue: 'http://127.0.0.1:8000',
+    defaultValue: 'https://econnect-server-6uhpsgm7fq-as.a.run.app',
   );
 }
